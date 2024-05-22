daf25be97f4e9d12455bd546e292f6faf3f3795a
SuJeet Mehra <82982529+sujeetmehra01@users.noreply.github.com>
2024-05-22 16:19:50 +0530

    pages and routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,37 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import CourseDetails from "./pages/CourseDetails";
+import Dashboard from "./pages/Dashboard";
+import Error from "./pages/Error";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
+import SearchCourse from "./pages/SearchCourse";
+import SignUp from "./pages/SignUp";
+import VerifyOtp from "./pages/VerifyOtp";
+import ViewCourse from "./pages/ViewCourse";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/homepage" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<ContactUs />} />
+        <Route path="/courses" element={<CourseDetails />} />
+        <Route path="/update-password" element={<ResetPassword />} />
+        <Route path="/search" element={<SearchCourse />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/verify-email" element={<VerifyOtp />} />
+        <Route path="/course-view" element={<ViewCourse />} />
       </Routes>
     </div>
   );
